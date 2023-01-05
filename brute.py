47ea66d042b0185b930d84d5ce03a5d5e75d0585
Marcin Wierzbiński <marcin119a@gmail.com>
2023-01-05 15:17:55 +0100

    Create brute.py

---
+++ brute.py
@@ -0,0 +1,26 @@
+def scs(ss):
+    """ Returns shortest common superstring of given
+        strings, which must be the same length """
+    shortest_sup = None
+    for ssperm in itertools.permutations(ss):
+        sup = ssperm[0]  # superstring starts as first string
+        for i in range(len(ss)-1):
+            # overlap adjacent strings A and B in the permutation
+            olen = overlap(ssperm[i], ssperm[i+1], min_length=1)
+            # add non-overlapping portion of B to superstring
+            sup += ssperm[i+1][olen:]
+        if shortest_sup is None or len(sup) < len(shortest_sup):
+            shortest_sup = sup  # found shorter superstring
+    return shortest_sup  # return shortest
+def pick_maximal_overlap(reads, k):
+    """ Return a pair of reads from the list with a
+        maximal suffix/prefix overlap >= k.  Returns
+        overlap length 0 if there are no such overlaps."""
+    reada, readb = None, None
+    best_olen = 0
+    for a, b in itertools.permutations(reads, 2):
+        olen = overlap(a, b, min_length=k)
+        if olen > best_olen:
+            reada, readb = a, b
+            best_olen = olen
+    return reada, readb, best_olen
