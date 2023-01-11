94ecc88d7b4bd6412e9178f1a35cb8874a43c52f
Marcin Wierzbiński <marcin119a@gmail.com>
2023-01-11 17:53:52 +0100

    Update dynamik.py

--- dynamik.py
+++ dynamik.py
@@ -20,12 +20,33 @@ for x,sx in enumerate(['A', 'C', 'G', 'T', '-']):
 x = 'CTCGGCCCTAGG'
 y = 'GGCTCTAGGCCC'
 
-D = np.zeros((len(x)+1, len(y)+1))
 
-for i in range(1,D.shape[1]):
-  D[0][i] = np.Inf
+def overlap(x, y, min_l=4)
+  D = np.zeros((len(x)+1, len(y)+1))
 
-for i in range(1, D.shape[0]):
-  for j in range(1, D.shape[1]):
-    D[i][j] = min(D[i-1,j] + s(x[i-1], '-'), D[i,j-1] + s('-', y[j-1]), D[i-1,j-1]+ s(x[i-1], y[j-1])) 
+  for i in range(1,D.shape[1]):
+    D[0][i] = np.Inf
 
+  for i in range(1, D.shape[0]):
+    for j in range(1, D.shape[1]):
+      D[i][j] = min(D[i-1,j] + s(x[i-1], '-'), D[i,j-1] + s('-', y[j-1]), D[i-1,j-1]+ s(x[i-1], y[j-1])) 
+
+
+  for j in range(D.shape[0]-min_l,D.shape[0]):
+    D[j][0] = np.Inf
+  for i in range(5):
+    D[D.shape[1]-1][i] = np.Inf
+
+  for i in range(D.shape[0]-min_l+1,D.shape[0]-1):
+    for j in range(1,min_l+1):
+      D[i][j] = min(D[i-1,j] + s(x[i-1], '-'), D[i,j-1] + s('-', y[j-1]), D[i-1,j-1]+ s(x[i-1], y[j-1])) 
+
+  xf = ""
+  yf = ""
+  for i,j in zip(range(np.argmin(D[len(y),:]),0, -1), range(len(x)-1, 0, -1)):
+    xf += f"{y[i-1]}"
+    yf += f"{x[j]}"
+    if y[i-1] == x[j]:
+      score += 0
+
+  return score, xf, yf
