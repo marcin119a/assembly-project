629b78480f62b35aa5da1006652ab34b0656020a
Marcin Wierzbiński <marcin119a@gmail.com>
2023-01-11 12:46:39 +0100

    Create dynamik.py

---
+++ dynamik.py
@@ -0,0 +1,31 @@
+import numpy as np 
+
+def s(a,b):
+  if a == '-' or b == '-':
+    return 8
+  if a == b:
+    return 0 
+  if (a == 'A' and b != 'G') or (a == 'C' and b != 'T') or (a == 'G' and b != 'A') or (a == 'T' and b != 'C'): 
+    return 4
+  else:
+    return 2
+    
+import numpy as np 
+r = np.zeros((5,5)) * 1
+for x,sx in enumerate(['A', 'C', 'G', 'T', '-']):
+  for y,sy in enumerate(['A', 'C', 'G', 'T', '-']):
+    r[x][y] = s(sx,sy)
+    
+    
+x = 'CTCGGCCCTAGG'
+y = 'GGCTCTAGGCCC'
+
+D = np.zeros((len(x)+1, len(y)+1))
+
+for i in range(1,D.shape[1]):
+  D[0][i] = np.Inf
+
+for i in range(1, D.shape[0]):
+  for j in range(1, D.shape[1]):
+    D[i][j] = min(D[i-1,j] + s(x[i-1], '-'), D[i,j-1] + s('-', y[j-1]), D[i-1,j-1]+ s(x[i-1], y[j-1])) 
+
